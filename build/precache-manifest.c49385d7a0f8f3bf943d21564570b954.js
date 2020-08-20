16512c4423ee00fb5e53190e182d638e8b1d0f9b
dan amouyal <danamouyal@hotmail.fr>
2020-08-20 17:29:04 +0300

    add build

---
+++ build/precache-manifest.c49385d7a0f8f3bf943d21564570b954.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa0f8eefef57650f32cd9562fb3e566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ceb40569aea0a5311e",
+    "url": "/static/css/2.6e5bf9e1.chunk.css"
+  },
+  {
+    "revision": "ca0048991c43065b0cba",
+    "url": "/static/css/main.8a4cb5f7.chunk.css"
+  },
+  {
+    "revision": "b9ceb40569aea0a5311e",
+    "url": "/static/js/2.9daf6ec8.chunk.js"
+  },
+  {
+    "revision": "ca0048991c43065b0cba",
+    "url": "/static/js/main.b4ff582c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
