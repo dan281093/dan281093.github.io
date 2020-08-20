25b330a4031cd5469ec2c1ea0c0920daadc42673
dan amouyal <danamouyal@hotmail.fr>
2020-08-20 17:54:14 +0300

    ready to deploy

--- src/elements/About.jsx
+++ src/elements/About.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import PageHelmet from "../component/common/Helmet";
 import Breadcrumb from "../elements/common/Breadcrumb";
-import CounterOne from "../elements/counters/CounterOne";
-import Testimonial from "../elements/Testimonial";
-import BrandTwo from "../elements/BrandTwo";
-import { FaFacebookF , FaLinkedinIn , FaTwitter } from "react-icons/fa";
+// import CounterOne from "../elements/counters/CounterOne";
+// import Testimonial from "../elements/Testimonial";
+// import BrandTwo from "../elements/BrandTwo";
+// import { FaFacebookF , FaLinkedinIn , FaTwitter } from "react-icons/fa";
 import ScrollToTop from 'react-scroll-up';
 import { FiChevronUp } from "react-icons/fi";
 import Header from "../component/header/Header";
